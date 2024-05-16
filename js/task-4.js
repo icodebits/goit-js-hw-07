6c7a319230a14c2fa38ddbdca0d667054eecb49e
Serghei Filimonov <filimonov.sergey@gmail.com>
2024-05-16 10:58:46 +0300

    Add Task 4.

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('.login-form');
+const submitButton = loginForm.querySelector('button[type="submit"]');
 
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const emailValue = loginForm.elements.email.value.trim();
+  const passwordValue = loginForm.elements.password.value.trim();
+
+  if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+  } else {
+    const formData = {
+      email: emailValue,
+      password: passwordValue
+    };
+
+    console.log(formData);
+
+    loginForm.reset();
+  }
+});
